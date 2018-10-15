bb97aff4c663385df5a55b8f184f37339d61e935
neil.mispelaar <neil.mispelaar@outlook.com>
2018-10-15 11:19:47 -0400

    Polish styling of the mobile menu

--- themes/hugo-wed-lab-v2/static/js/plugins.js
+++ themes/hugo-wed-lab-v2/static/js/plugins.js
@@ -134,7 +134,7 @@ var myMobileMenu = (function () {
 
 		// Add the Menu Close Button
 		menuContainer.append(
-			"<button class=\"btn menuCloseButton\" type=\"button\">Close</button>" + "</div>")
+			"<button class=\"btn btn-red menuCloseButton\" type=\"button\">Close</button>" + "</div>")
 
 		// Cache the buttons
 		menuOpenButton = wrapper.find('.menuOpenButton');
